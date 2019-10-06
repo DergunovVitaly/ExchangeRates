225e9b1f85aa15fa1f4da7337ec1f9636669cab2
Denys Melnykov <deniska228@gmail.com>
2019-10-06 11:39:27 +0200

    install snap kit

--- ExchangeRates/ExchangeRates/Application/AppDelegate.swift
+++ ExchangeRates/ExchangeRates/Application/AppDelegate.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import SnapKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyBoard.instantiateInitialViewController()
+        self.window?.rootViewController = viewController
+        self.window?.makeKeyAndVisible()
         return true
     }
 }
--- ExchangeRates/ExchangeRates/UserStories/Tutorial/TutorialView.swift
+++ ExchangeRates/ExchangeRates/UserStories/Tutorial/TutorialView.swift
@@ -10,5 +10,18 @@ import UIKit
 
 class TutorialView: UIView {
 
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        
+    }
+    
 }

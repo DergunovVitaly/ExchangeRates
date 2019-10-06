964346b7fccdf8038fb0f2af56b9a9c97c3b445a
Denys Melnykov <deniska228@gmail.com>
2019-10-06 10:43:30 +0200

    setup project

---
+++ ExchangeRates/ExchangeRates/UserStories/Tutorial/TutorialView.swift
@@ -0,0 +1,14 @@
+//
+//  TutorialView.swift
+//  ExchangeRates
+//
+//  Created by Mac on 06/10/2019.
+//  Copyright © 2019 Denis Melnikov. All rights reserved.
+//
+
+import UIKit
+
+class TutorialView: UIView {
+
+
+}

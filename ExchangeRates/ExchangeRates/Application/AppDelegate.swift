71506203a1f5d7dee08c96ffc0a0b3993df2fb74
Denys Melnykov <deniska228@gmail.com>
2019-11-01 12:15:37 +0100

    Show tutorial screen once, when first start application

--- ExchangeRates/ExchangeRates/Application/AppDelegate.swift
+++ ExchangeRates/ExchangeRates/Application/AppDelegate.swift
@@ -13,13 +13,33 @@ import SnapKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
+    var optionallyStoreTheFirstLaunchFlag = false
+    
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let viewController = TutorialVC()
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
+        optionallyStoreTheFirstLaunchFlag = UserDefaults.isFirstLaunch()
+        let firstLaunch = optionallyStoreTheFirstLaunchFlag
+        if firstLaunch {
+            window?.rootViewController = TutorialVC()
+        } else {
+            window?.rootViewController = BanksVC()
+        }
         return true
     }
 }
 
+extension UserDefaults {
+    static func isFirstLaunch() -> Bool {
+        let hasBeenLaunchedBeforeFlag = "hasBeenLaunchedBeforeFlag"
+        let isFirstLaunch = !UserDefaults.standard.bool(forKey: hasBeenLaunchedBeforeFlag)
+        if (isFirstLaunch) {
+            UserDefaults.standard.set(true, forKey: hasBeenLaunchedBeforeFlag)
+            UserDefaults.standard.synchronize()
+        }
+        return isFirstLaunch
+    }
+}

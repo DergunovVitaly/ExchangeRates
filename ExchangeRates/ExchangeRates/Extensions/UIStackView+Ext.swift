9c373f1c06949d873eb2fc0722833f02e763b20a
Denys Melnykov <deniska228@gmail.com>
2019-11-01 12:54:56 +0100

    no message

--- ExchangeRates/ExchangeRates/Extensions/UIStackView+Ext.swift
+++ ExchangeRates/ExchangeRates/Extensions/UIStackView+Ext.swift
@@ -17,15 +17,5 @@ extension UIStackView {
     }
 }
 
-extension UserDefaults {
-    static func isFirstLaunch() -> Bool {
-        let hasBeenLaunchedBeforeFlag = "hasBeenLaunchedBeforeFlag"
-        let isFirstLaunch = !UserDefaults.standard.bool(forKey: hasBeenLaunchedBeforeFlag)
-        if (isFirstLaunch) {
-            UserDefaults.standard.set(true, forKey: hasBeenLaunchedBeforeFlag)
-            UserDefaults.standard.synchronize()
-        }
-        return isFirstLaunch
-    }
-}
+
 

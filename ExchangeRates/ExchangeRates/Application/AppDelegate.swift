2d3b63d84b9216c405ced60f3221a5db0a76b0c3
Denys Melnykov <deniska228@gmail.com>
2019-11-01 12:52:28 +0100

    move extentson userDefaults in Extension folder

--- ExchangeRates/ExchangeRates/Application/AppDelegate.swift
+++ ExchangeRates/ExchangeRates/Application/AppDelegate.swift
@@ -32,14 +32,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 }
 
-extension UserDefaults {
-    static func isFirstLaunch() -> Bool {
-        let hasBeenLaunchedBeforeFlag = "hasBeenLaunchedBeforeFlag"
-        let isFirstLaunch = !UserDefaults.standard.bool(forKey: hasBeenLaunchedBeforeFlag)
-        if (isFirstLaunch) {
-            UserDefaults.standard.set(true, forKey: hasBeenLaunchedBeforeFlag)
-            UserDefaults.standard.synchronize()
-        }
-        return isFirstLaunch
-    }
-}
--- ExchangeRates/ExchangeRates/Extensions/UIStackView+Ext.swift
+++ ExchangeRates/ExchangeRates/Extensions/UIStackView+Ext.swift
@@ -16,3 +16,16 @@ extension UIStackView {
         insertSubview(subView, at: 0)
     }
 }
+
+extension UserDefaults {
+    static func isFirstLaunch() -> Bool {
+        let hasBeenLaunchedBeforeFlag = "hasBeenLaunchedBeforeFlag"
+        let isFirstLaunch = !UserDefaults.standard.bool(forKey: hasBeenLaunchedBeforeFlag)
+        if (isFirstLaunch) {
+            UserDefaults.standard.set(true, forKey: hasBeenLaunchedBeforeFlag)
+            UserDefaults.standard.synchronize()
+        }
+        return isFirstLaunch
+    }
+}
+
